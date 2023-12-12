0eb21e1f8fc60105e75e909ce394004e17612e07
Ariv Debnath <arivdebnath10@gmail.com>
2023-12-12 19:34:03 +0530

    adds error handling

--- index.js
+++ index.js
@@ -7,30 +7,37 @@ app.use(express.json());
 
 const URL = process.env.URL;
 
-app.get('/convert', async (req, res)=>{
-    const {fromCurrency, toCurrency, date} = req.body;
-
-    const fromData = await axios.request({
-        method: "GET",
-        url: URL + "/coins/" + fromCurrency + "/history",
-        params: {
-            date,
-        }
-    });
-    const toData = await axios.request({
-        method: "GET",
-        url: URL + "/coins/" + toCurrency + "/history",
-        params: {
-            date,
-        }
-    });
-    const fromCurrencyUSD = fromData.data.market_data.current_price.usd;
-    const toCurrencyUSD = toData.data.market_data.current_price.usd;
-    const convertedPrice = fromCurrencyUSD/toCurrencyUSD;
-    res.status(200).json({ price: convertedPrice});
+app.get('/convert', async (req, res) => {
+    try {
+        let { fromCurrency, toCurrency, date } = req.body;
+        fromCurrency = fromCurrency.toLowerCase();
+        toCurrency = toCurrency.toLowerCase();
+        const fromData = await axios.request({
+            method: "GET",
+            url: URL + "/coins/" + fromCurrency + "/history",
+            params: {
+                date,
+            }
+        });
+        const toData = await axios.request({
+            method: "GET",
+            url: URL + "/coins/" + toCurrency + "/history",
+            params: {
+                date,
+            }
+        });
+        const fromCurrencyUSD = fromData.data.market_data.current_price.usd;
+        const toCurrencyUSD = toData.data.market_data.current_price.usd;
+        const convertedPrice = fromCurrencyUSD / toCurrencyUSD;
+        return res.status(200).json({ price: convertedPrice });
+    } catch (err) {
+        return res.status(500).json({
+            error: err,
+        });
+    }
 })
 
 const PORT = 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
